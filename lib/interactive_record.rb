c481e7681f262fd231d57b832914387c93a61c4a
Jane Venter <janeanneventer@gmail.com>
2019-02-09 04:06:00 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,7 +51,7 @@ class InteractiveRecord
   
   # def self.find_by(attribute)
     value = "'#{attribute.to_a[0][1].to_s}'"
-  #   sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.to_a[0][0].to_s} = '#{attribute.to_a[0][1].to_s}'"
+  #   sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.to_a[0][0].to_s} = #{value}"
   #   #binding.pry
   #   DB[:conn].execute(sql)
   # end
